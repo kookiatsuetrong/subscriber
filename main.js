2b480f1c3b533fd4da50b0b62f78ce4936dc2922
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2018-11-13 14:02:46 +0000

    Render HTML

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+// https://github.com/kookiatsuetrong/subscriber
 var express  = require('express')
 var server   = express()
 var ejs      = require('ejs')
 var mysql    = require('mysql')
 var database = { host:'localhost', database: 'web',
                  user:'james',     password: 'bond'}
+server.engine('html', ejs.renderFile)
 server.listen(80)
 server.get('/apply', showApplyPage)
 
@@ -11,7 +13,8 @@ function showApplyPage(req, res) {
   var conn = mysql.createConnection(database)
   conn.connect()
   conn.query('select * from service', function(error, data) {
-    res.send(data)
+    // res.send(data) // for web service
+    res.render('apply.html', {service: data})
     conn.end()
   })
 }
