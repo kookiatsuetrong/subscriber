98450ca34a20c8641a8c7db1ee3702dce57d0276
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2018-11-13 13:55:53 +0000

    Read service from database

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// Main App
+var express  = require('express')
+var server   = express()
+var ejs      = require('ejs')
+var mysql    = require('mysql')
+var database = { host:'localhost', database: 'web',
+                 user:'james',     password: 'bond'}
+server.listen(80)
+server.get('/apply', showApplyPage)
+
+function showApplyPage(req, res) {
+  var conn = mysql.createConnection(database)
+  conn.connect()
+  conn.query('select * from service', function(error, data) {
+    res.send(data)
+    conn.end()
+  })
+}
