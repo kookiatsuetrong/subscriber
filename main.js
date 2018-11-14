b4be95327abab00a4f93540577a693134a622483
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2018-11-14 13:55:32 +0000

    Add user information to database

--- main.js
+++ main.js
@@ -19,6 +19,20 @@ function showApplyPage(req, res) {
       conn.end()
     })
   } else {
-    res.send(req.query)
+    var phone = req.query.phone || ''
+    var name  = req.query.name  || ''
+    var address = req.query.address || ''
+    if (phone == '') {
+      res.redirect('/apply')
+    } else {
+      var sql = "insert into member(phone,name,address) values"+
+                "(?,?,?)"
+      var conn = mysql.createConnection(database)
+      conn.connect()
+      conn.query(sql, [phone, name, address], function() {
+        res.redirect('/apply')
+        conn.end()
+      })
+    }
   }
 }
